1180592f743ea78a38b69da6d7a05058cf264fd2
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2020-12-31 12:06:13 +0200

    Include frontend build (Task 3.11).

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+
 //app.use(morgan('tiny'))
 app.use(morgan(function (tokens, req, res) { 
   const body = JSON.stringify(req.body)
